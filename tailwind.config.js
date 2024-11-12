c781681e60d23109f1c5ba5aa6986e9cdffa845e
Moustafa-Ashraf04 <moustafa.ashraf277@gmail.com>
2024-11-12 12:12:43 +0200

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  corePlugins: {
+    preflight: false,
+  },
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#EB1F2F",
+        grayTitle: "#848484",
+        grayPar: "#888888",
+      },
+    },
+  },
+  plugins: [],
+}
+
