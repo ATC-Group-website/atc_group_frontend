9d3f3bf6f9752daed2f03e6a740b05c586a9776b
Moustafa-Ashraf04 <moustafa.ashraf277@gmail.com>
2024-11-24 01:15:27 +0200

    add: about us page

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import { TopBarComponent } from "../../shared/components/top-bar/top-bar.component";
-import { NavBarComponent } from "../../shared/components/nav-bar/nav-bar.component";
+import { TopBarComponent } from '../../shared/components/top-bar/top-bar.component';
+import { NavBarComponent } from '../../shared/components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-services',
   standalone: true,
   imports: [TopBarComponent, NavBarComponent],
   templateUrl: './services.component.html',
-  styleUrl: './services.component.css'
+  styleUrl: './services.component.css',
 })
-export class ServicesComponent {
-
-}
+export class ServicesComponent {}
