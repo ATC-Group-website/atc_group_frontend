f6c0e1e06c6a777db2869883b233fe1e6903e3f5
Moustafa-Ashraf04 <moustafa.ashraf277@gmail.com>
2024-11-24 13:44:56 +0200

    about us & our team v1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,22 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { ServicesComponent } from './pages/services/services.component';
+import { OurTeamComponent } from './pages/about-us/our-team/our-team.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'about-us', component: AboutUsComponent },
+  {
+    path: 'about-us',
+    component: AboutUsComponent,
+  },
+  {
+    path: 'about-us/our-team',
+
+    loadComponent: () =>
+      import('./pages/about-us/our-team/our-team.component').then(
+        (mod) => mod.OurTeamComponent,
+      ),
+  },
   { path: 'services', component: ServicesComponent },
   { path: '**', redirectTo: '' }, // Optional: Redirects unknown paths to the home route
 ];
