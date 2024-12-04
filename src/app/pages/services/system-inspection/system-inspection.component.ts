6792594a5830255b18b8b4dcfca5a3b595b83c3d
Moustafa-Ashraf04 <moustafa.ashraf277@gmail.com>
2024-12-04 13:53:20 +0200

    update margin in ul in single services

--- src/app/pages/services/system-inspection/system-inspection.component.ts
+++ src/app/pages/services/system-inspection/system-inspection.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { TopBarComponent } from "../../../shared/components/top-bar/top-bar.component";
-import { NavBarComponent } from "../../../shared/components/nav-bar/nav-bar.component";
-import { ScrollToTopComponent } from "../../../shared/components/scroll-to-top/scroll-to-top.component";
-import { FooterComponent } from "../../../shared/components/footer/footer.component";
+import { TopBarComponent } from '../../../shared/components/top-bar/top-bar.component';
+import { NavBarComponent } from '../../../shared/components/nav-bar/nav-bar.component';
+import { ScrollToTopComponent } from '../../../shared/components/scroll-to-top/scroll-to-top.component';
+import { FooterComponent } from '../../../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-system-inspection',
   standalone: true,
-  imports: [TopBarComponent, NavBarComponent, ScrollToTopComponent, FooterComponent],
+  imports: [
+    TopBarComponent,
+    NavBarComponent,
+    ScrollToTopComponent,
+    FooterComponent,
+  ],
   templateUrl: './system-inspection.component.html',
   styleUrl: './system-inspection.component.css',
 })
--- src/app/pages/services/transfer-pricing/transfer-pricing.component.ts
+++ src/app/pages/services/transfer-pricing/transfer-pricing.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
-import { TopBarComponent } from "../../../shared/components/top-bar/top-bar.component";
-import { NavBarComponent } from "../../../shared/components/nav-bar/nav-bar.component";
-import { ScrollToTopComponent } from "../../../shared/components/scroll-to-top/scroll-to-top.component";
-import { FooterComponent } from "../../../shared/components/footer/footer.component";
+import { TopBarComponent } from '../../../shared/components/top-bar/top-bar.component';
+import { NavBarComponent } from '../../../shared/components/nav-bar/nav-bar.component';
+import { ScrollToTopComponent } from '../../../shared/components/scroll-to-top/scroll-to-top.component';
+import { FooterComponent } from '../../../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-transfer-pricing',
   standalone: true,
-  imports: [TopBarComponent, NavBarComponent, ScrollToTopComponent, FooterComponent],
+  imports: [
+    TopBarComponent,
+    NavBarComponent,
+    ScrollToTopComponent,
+    FooterComponent,
+  ],
   templateUrl: './transfer-pricing.component.html',
-  styleUrl: './transfer-pricing.component.css'
+  styleUrl: './transfer-pricing.component.css',
 })
-export class TransferPricingComponent {
-
-}
+export class TransferPricingComponent {}
