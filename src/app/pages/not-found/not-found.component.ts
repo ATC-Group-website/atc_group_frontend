277d08bd0841e293bcdc34672ed6acc81af03d8a
Moustafa-Ashraf04 <moustafa.ashraf277@gmail.com>
2024-12-11 11:18:51 +0200

    not found page & update background color of overview section

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TopBarComponent } from '../../shared/components/top-bar/top-bar.component';
 import { NavBarComponent } from '../../shared/components/nav-bar/nav-bar.component';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
+import { Location } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [TopBarComponent, NavBarComponent, FooterComponent],
+  imports: [TopBarComponent, NavBarComponent, FooterComponent, RouterModule],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css',
 })
-export class NotFoundComponent {}
+export class NotFoundComponent {
+  location = inject(Location);
+
+  goBack(): void {
+    this.location.back();
+  }
+}
